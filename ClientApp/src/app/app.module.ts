1db8a546c319ddf8affe4e00c574e0123d1be6d8
Dominik Voss <dominik.voss@check24.de>
2020-03-21 17:20:33 +0100

    added angular material added global layout component added header and burger menu

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LoadingBarModule} from '@ngx-loading-bar/core';
-import {LoadingBarHttpClientModule} from '@ngx-loading-bar/http-client';
-import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,6 +26,9 @@ import {AppComponent} from './app.component';
 
     HttpClientModule,
     BrowserAnimationsModule,
+
+    MatMenuModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
