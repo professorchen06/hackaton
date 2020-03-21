7fc34e45f782afaa93b1431de9c2509c75d06f7c
philippmeissner <pm@zeit.io>
2020-03-21 15:15:28 +0100

    added loading bar and modules

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,10 @@
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoadingBarModule} from '@ngx-loading-bar/core';
+import {LoadingBarHttpClientModule} from '@ngx-loading-bar/http-client';
+import {LoadingBarRouterModule} from '@ngx-loading-bar/router';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
@@ -12,6 +16,11 @@ import {AppComponent} from './app.component';
     BrowserModule,
     AppRoutingModule,
 
+    LoadingBarHttpClientModule,
+    LoadingBarRouterModule,
+    LoadingBarModule,
+
+    HttpClientModule,
     BrowserAnimationsModule,
   ],
   providers: [],
