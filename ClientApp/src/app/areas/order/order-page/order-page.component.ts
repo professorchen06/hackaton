1ff175a9a58f754325281e5885a72e90f5b7e3f4
flobwer <bueschersflorian@gmail.com>
2020-03-21 17:53:46 +0100

    added teplate for adding order items form  and set order item to order

--- ClientApp/src/app/areas/order/order-page/order-page.component.ts
+++ ClientApp/src/app/areas/order/order-page/order-page.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Observable} from 'rxjs';
 import {OrderStates, OrderStateService} from './order-state.service';
+import { Order, OrderItem } from '../order.model';
 
 @Component({
   selector: 'app-order-page',
@@ -11,9 +12,15 @@ export class OrderPageComponent {
   states = OrderStates;
   readonly state: Observable<OrderStates>;
 
+  order: Order = {orderItems: []};
+
   constructor(
     private readonly orderStateService: OrderStateService,
   ) {
     this.state = orderStateService.observable();
+  }
+
+  addOrderItem(orderItem: OrderItem): void {
+    this.order.orderItems.push(orderItem);
   }
 }
