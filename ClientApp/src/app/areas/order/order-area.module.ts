9aa38721a8e4bff324088126bfc660443c1a644b
flobwer <bueschersflorian@gmail.com>
2020-03-21 19:23:20 +0100

    basic styling of the order form

--- ClientApp/src/app/areas/order/order-area.module.ts
+++ ClientApp/src/app/areas/order/order-area.module.ts
@@ -4,6 +4,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {OrderAreaRoutingModule} from './order-area-routing.module';
 import {OrderPageComponent} from './order-page/order-page.component';
 import { OrderFormComponent } from './order-page/order-form/order-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ItemListComponent } from 'src/app/item-list/item-list.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatSort, MatSortModule} from '@angular/material/sort';
@@ -13,6 +15,8 @@ import {MatSort, MatSortModule} from '@angular/material/sort';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatTableModule,
     MatSortModule,
   ],
