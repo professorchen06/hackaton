7fc7fbd6607096337a72655f81587da8fcb9848f
Dominik Voss <dominik.voss@check24.de>
2020-03-22 11:59:03 +0100

    Add first view with CTAs

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -13,12 +13,14 @@ import { BackendApiModule } from './core/backend-api/backend-api.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
+import { FirstViewComponent } from './first-view/first-view.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
+    FirstViewComponent,
   ],
   imports: [
     BrowserModule,
