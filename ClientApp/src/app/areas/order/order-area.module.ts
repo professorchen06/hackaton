3ef836f605cf9ee9634f3fb932b6ef3ce05c53fd
philippmeissner <pm@zeit.io>
2020-03-22 11:26:46 +0100

    added orders overview

--- ClientApp/src/app/areas/order/order-area.module.ts
+++ ClientApp/src/app/areas/order/order-area.module.ts
@@ -14,6 +14,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { OrdersOverviewComponent } from './order-overview/orders-overview.component';
 @NgModule({
   imports: [
     OrderAreaRoutingModule,
@@ -29,7 +30,13 @@ import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBar, MatSnackBarModule } from '@
     MatIconModule,
     MatSnackBarModule
   ],
-  declarations: [OrderPageComponent, OrderFormComponent, ItemListComponent, ConfirmOrderModalContentComponent],
+  declarations: [
+    OrderPageComponent,
+    OrderFormComponent,
+    ItemListComponent,
+    ConfirmOrderModalContentComponent,
+    OrdersOverviewComponent,
+  ],
   providers: [
     {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
   ]
